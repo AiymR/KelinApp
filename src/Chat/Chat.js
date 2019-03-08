a6fcc4fc1a0e4516b24abf2434d54290f87a6b6a
Aiym <raikhanovaaiym@gmail.com>
2019-03-09 00:25:14 +0600

    new chat

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Input } from 'semantic-ui-react'
 import './Chat.css';
 
 
@@ -8,9 +9,63 @@ class Chat extends Component {
         <div className="chat-page">
           <div className="chat-frame">
             <div className="left">
-              <h1>sdvf</h1>
+              <div className="top-searh">
+              <Input fluid icon='users' iconPosition='right' placeholder='Search users...' />
+            </div>
+            <div className="contacts">
+            <ul class="people">
+                <li class="person" data-chat="person1">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg" alt="" />
+                    <span class="name">Thomas Bangalter</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">I was wondering...</span>
+                </li>
+                <li class="person" data-chat="person2">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/dog.png" alt="" />
+                    <span class="name">Dog Woofson</span>
+                    <span class="time">1:44 PM</span>
+                    <span class="preview">I've forgotten how it felt before</span>
+                </li>
+                <li class="person" data-chat="person3">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg" alt="" />
+                    <span class="name">Louis CK</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">But we’re probably gonna need a new carpet.</span>
+                </li>
+                <li class="person" data-chat="person4">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/bo-jackson.jpg" alt="" />
+                    <span class="name">Bo Jackson</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">It’s not that bad...</span>
+                </li>
+                <li class="person" data-chat="person5">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/michael-jordan.jpg" alt="" />
+                    <span class="name">Michael Jordan</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">Wasup for the third time like is
+you blind bitch</span>
+                </li>
+                <li class="person" data-chat="person6">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/drake.jpg" alt="" />
+                    <span class="name">Drake</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">howdoyoudoaspace</span>
+                </li>
+            </ul>
             </div>
           </div>
+          <div className="right">
+            <div className="top-chat">
+            <span>To: user name</span>
+            </div>
+            <div className="messages">
+
+            </div>
+              <div  className="write">
+              <Input fluid icon='search' placeholder='Search...' />
+              </div>
+          </div>
+        </div>
       </div>
   );
 }
